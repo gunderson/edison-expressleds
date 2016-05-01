ad13294e1aca6cf2c48470625df52e2c71be19c7
Patrick Gunderson <patrick@toolofna.com>
2016-04-30 17:49:13 -0700

    dockerfilegit push resin

--- deps.sh
+++
@@ -1,5 +0,0 @@
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-apt-get update
-apt-get install imagemagick
-apt-get install -y mongodb-org
