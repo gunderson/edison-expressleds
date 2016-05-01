9b4b1166cf424064eb6f50021e37a8ecbb8daa9d
Patrick Gunderson <patrick@toolofna.com>
2016-04-30 19:04:41 -0700

    trying desparately to define deps.sh

--- deps.sh
+++ deps.sh
@@ -1,2 +1,7 @@
+# /etc/apt/sources.list.d/dotdeb.org.list
+deb http://packages.dotdeb.org squeeze all
+deb-src http://packages.dotdeb.org squeeze all
+wget -q -O - http://www.dotdeb.org/dotdeb.gpg | sudo apt-key add -
+
 apt-get update
-apt-get install nodejs redis nginx postgresql-9.4
+apt-get install nodejs redis-server nginx postgresql-9.4
