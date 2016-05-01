c81571720a4025265b5a6d03845a78e69f0333bb
Patrick Gunderson <patrick@toolofna.com>
2016-04-30 19:35:42 -0700

    added deps

--- deps.sh
+++ deps.sh
@@ -1,6 +1,6 @@
 # /etc/apt/sources.list.d/dotdeb.org.list
-deb http://packages.dotdeb.org squeeze all
-deb-src http://packages.dotdeb.org squeeze all
+echo "deb http://packages.dotdeb.org squeeze all" | sudo tee -a /etc/apt/sources.list.d/dotdeb.org.list
+echo "deb-src http://packages.dotdeb.org squeeze all" | sudo tee -a /etc/apt/sources.list.d/dotdeb.org.list
 wget -q -O - http://www.dotdeb.org/dotdeb.gpg | sudo apt-key add -
 
 apt-get update
