540096061054c0764c92cdb9d99967dfd4c627a8
Patrick Gunderson <patrick@toolofna.com>
2016-04-30 19:24:40 -0700

    trying desparately to define deps.sh

--- deps.sh
+++ deps.sh
@@ -4,4 +4,4 @@ deb-src http://packages.dotdeb.org squeeze all
 wget -q -O - http://www.dotdeb.org/dotdeb.gpg | sudo apt-key add -
 
 apt-get update
-apt-get install nodejs redis-server nginx postgresql-9.4
+apt-get install nodejs redis-server nginx postgresql-9.4 avahi-daemon netatalk
