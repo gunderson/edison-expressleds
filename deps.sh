837ca377c8ce72f557e527982af08cac8ecf3c89
Patrick Gunderson <patrick@toolofna.com>
2016-04-30 17:31:18 -0700

    updated deps again

--- deps.sh
+++ deps.sh
@@ -1,1 +1,5 @@
-apt-get install imagemagick && apt-get install mongodb-org
+echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo apt-get update
+apt-get install imagemagick
+apt-get install -y mongodb-org
