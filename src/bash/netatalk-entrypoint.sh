29ed73d578fdf0ea1292fe819561c96c9d415425
Patrick Gunderson <patrick@toolofna.com>
2016-05-01 19:08:13 -0700

    debugging avahi

--- src/bash/netatalk-entrypoint.sh
+++ src/bash/netatalk-entrypoint.sh
@@ -38,6 +38,8 @@ if [ "${AVAHI}" == "1" ]; then
     # Set host-name and enable-dbus
     sed -i -e "s@#enable-dbus=yes@enable-dbus=no@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_NAME | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
 
+    echo "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_NAME | cut -c1-7)@"
+
     # Restart service
     /etc/init.d/avahi-daemon restart
 
