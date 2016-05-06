096f1929ab9bddce13dab644c55940bae8dc4588
Patrick Gunderson <patrick@toolofna.com>
2016-05-05 17:12:06 -0700

    better debugging

--- src/front-end/js/index.js
+++ src/front-end/js/index.js
@@ -1,6 +1,6 @@
-var $ = require("zepto");
-var _ = require("lodash");
-
-
-$("button.play").click(() => $.get("http://http://192.168.6.249:3000/play"));
-$("button.stop").click(() => $.get("http://http://192.168.6.249:3000/stop"));
+// var $ = require("zepto");
+// var _ = require("lodash");
+//
+//
+// $("button.play").click(() => $.get("http://192.168.6.249:3000/play"));
+// $("button.stop").click(() => $.get("http://192.168.6.249:3000/stop"));
