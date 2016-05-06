f0653f57416f7e2506fcdabe1045550a5b25be22
Patrick Gunderson <patrick@toolofna.com>
2016-05-06 12:40:38 -0700

    updated jquery scripts

--- src/front-end/js/control-panel-view.js
+++ src/front-end/js/control-panel-view.js
@@ -1,19 +1,22 @@
 var $ = require("jquery");
 var _ = require("lodash");
-var io = require("socket.io-client");
+// var io = require("socket.io-client");
 
-var socket = io('http://localhost');
-socket.on('connect', function() {});
-socket.on('event', function(data) {});
-socket.on('disconnect', function() {});
+// var socket = io('http://localhost');
+// socket.on('connect', function() {});
+// socket.on('event', function(data) {});
+// socket.on('disconnect', function() {});
 
-$("button.play").click(() => $.get("http://10.0.1.8:3000/play"));
-$("button.stop").click(() => $.get("http://10.0.1.8:3000/stop"));
+var localIp = "192.168.6.249";
+var localPort = "3000";
+
+$("button.play").click(() => $.get(`http://${localIp}:${localPort}/play`));
+$("button.stop").click(() => $.get(`http://${localIp}:${localPort}/stop`));
 
 $("input[type=checkbox]").change((el) => {
     if (el.checked) {
-        $.get(`http://10.0.1.8:3000/led/${ el.value }/on`);
+        $.get(`http://${localIp}:${localPort}/led/${ el.value }/on`);
     } else {
-        $.get(`http://10.0.1.8:3000/led/${ el.value }/off`);
+        $.get(`http://${localIp}:${localPort}/led/${ el.value }/off`);
     }
 });
