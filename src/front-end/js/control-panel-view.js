9ebecd5990d830e2c84c684d4a39b257c6e227e0
Patrick Gunderson <patrick@toolofna.com>
2016-05-05 23:09:26 -0700

    new code

--- src/front-end/js/control-panel-view.js
+++ src/front-end/js/control-panel-view.js
@@ -2,22 +2,19 @@ var $ = require("jquery");
 var _ = require("lodash");
 var io = require("socket.io-client");
 
-
 var socket = io('http://localhost');
-  socket.on('connect', function(){});
-  socket.on('event', function(data){});
-  socket.on('disconnect', function(){});
-
+socket.on('connect', function() {});
+socket.on('event', function(data) {});
+socket.on('disconnect', function() {});
 
 $("button.play").click(() => $.get("http://10.0.1.8:3000/play"));
 $("button.stop").click(() => $.get("http://10.0.1.8:3000/stop"));
 
-
 $("input[type=checkbox]").change((el) => {
     var $el = $(el);
-    if ($el.is(":checked")){
-        $.get(`http://10.0.1.8:3000/led/${ $el.val() }/on`));
+    if ($el.is(":checked")) {
+        $.get(`http://10.0.1.8:3000/led/${ $el.val() }/on`);
     } else {
-        $.get(`http://10.0.1.8:3000/led/${ $el.val() }/off`));
+        $.get(`http://10.0.1.8:3000/led/${ $el.val() }/off`);
     }
-}
+});
