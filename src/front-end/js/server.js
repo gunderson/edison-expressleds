0fd7c78b558415f98b017d8d6b0db1fe007281fd
Patrick Gunderson <patrick@toolofna.com>
2016-05-08 19:41:37 -0700

    derped the dist folder

--- src/front-end/js/server.js
+++ src/front-end/js/server.js
@@ -7,42 +7,26 @@ var favicon = require( "serve-favicon" );
 var logger = require( "morgan" );
 var methodOverride = require( "method-override" );
 var path = require( "path" );
-var sassMiddleware = require( "node-sass-middleware" );
-var sass = require( "node-sass" );
 
 var app = express();
 app = _.extend( app, BackboneEvents );
+app.set( "env", "development" );
+app.use( express.static( path.resolve( __dirname, "../../../dist/front-end/" ) ) );
+
 var router = express.Router();
 router.use( logger( 'dev' ) );
 router.use( methodOverride() );
 
-app.set( "env", "development" );
 // parse application/x-www-form-urlencoded
 router.use( bodyParser.urlencoded( {
 	extended: false,
 } ) );
 // parse application/json
 router.use( bodyParser.json() );
-//
-// app.set( 'views', path.resolve( __dirname, '../jade/static/' ) );
-// app.set( 'view engine', 'pug' );
-//
-// app.use(
-// 	sassMiddleware( {
-// 		src: path.resolve( __dirname, '../sass' ),
-// 		dest: path.resolve( __dirname, '../css' ),
-// 		prefix: '/css',
-// 		debug: true,
-// 		indentedSyntax: true,
-// 		sourceMap: true,
-// 	} )
-// );
-
-app.use( express.static( path.resolve( __dirname, "../../../dist/front-end/" ) ) );
 
 app.listen( 80, function () {
 	console.log( 'Front-end server listening on port 80!', path.resolve( __dirname, "../../../dist/front-end/" ) );
 } );
-// app.use( '/', router );
+app.use( '/', router );
 
 module.exports = app;
